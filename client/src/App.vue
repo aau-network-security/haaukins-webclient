c2925af9b3617f288b0f83ade216d965ba2bd16f
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2019-11-20 11:34:28 +0100

    Made connection with Haaukins in my localhost

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <input v-model="inputField" v-on:keyup.enter="listEvent()" placeholder="Your Name..">
+    <button @click="login()" class="btn btn-primary">login</button>
+    <button @click="listEvent()" class="btn btn-primary">List events</button>
+    <br>
     <span>{{resp}}</span>
+    <span>{{token}}</span>
+    <span>{{err}}</span>
   </div>
 </template>
 
 <script>
-  import {HelloRequest} from "proto_pb"; //the Js file contains the messages defined in the proto file
-  import {GreeterClient} from "proto_grpc_web_pb"  //The Js file contains the service and the rpc call defined in the proto file
+  import {LoginUserRequest, ListEventsRequest} from "daemon_pb"; //the Js file contains the messages defined in the proto file
+  import {DaemonClient} from "daemon_grpc_web_pb"  //The Js file contains the service and the rpc call defined in the proto file
 
 export default {
   name: 'app',
@@ -15,26 +21,38 @@ export default {
   data: function() {
     return {
       inputField: "",
-      resp: "None"
+      resp: "aaa",
+      err: "",
+      dummy: "",
+      token: "",
     };
   },
   created: function() {
-    this.client = new GreeterClient("http://localhost:8080", null, null);
-    this.yayHello();
+    this.client = new DaemonClient("http://localhost:8080", null, null);
+    //this.login();
   },
   methods: {
-    yayHello: function () {
+    login: function () {
 
       //Create an HelloRequest made in the proto file
-      let getRequest = new HelloRequest();
-
-      //Set the name defined in the proto file (Name with capital letter)
-      getRequest.setName('World');
+      let getRequest = new LoginUserRequest();
+      getRequest.setUsername("gian");
+      getRequest.setPassword("bomber1");
 
       //sayHello is the function declared in the `proto_grpc_web_pb.js` file. It is the function declared in the proto.proto file
       // it is not clear why it is not capital letter.
-      this.client.sayHello(getRequest, {}, (err, response) => {
+      this.client.loginUser(getRequest, {}, (err, response) => {
+        this.err = err;
+        this.token = response;
+      });
+    },
+    listEvent: function () {
+      let getRequest = new ListEventsRequest();
+      let token = this.token;
+      this.client.listEvents(getRequest, {Token: token}, (err, response) => {
         this.resp = response;
+        this.err = err;
+        window.console.log(response)
       });
     }
   }
