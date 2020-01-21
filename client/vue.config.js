2eb1700b64c301b617326a7cd54bc8d0151ba4f4
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-01-21 16:32:52 +0100

    latest configs for haaukins-webclient interaction

---
+++ client/vue.config.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+module.exports = {
+	  devServer: {
+		      host: 'admin.ntp-event.dk',
+		      port:  '8000',
+		      https: {
+		         key: fs.readFileSync('./privkey.pem'),
+		         cert: fs.readFileSync('./fullchain.pem'),
+	  		},
+		      public: 'https://localhost:8000' 
+		    }
+}
