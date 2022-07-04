3cc5d896b3f456298047b0d4e1c7790ac2d4b8a9
mrtrkmn <mr.turkmen@icloud.com>
2022-07-04 20:38:28 +0200

    signup endpoint moved to rest

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,4 +8,5 @@
         name: 'app'
     };
     export const daemonclient = new DaemonClient(window.location.protocol+"//"+window.location.hostname+":8000", null, null);
+    export const API_ENDPOINT = 'http://localhost:8090'
 </script>
