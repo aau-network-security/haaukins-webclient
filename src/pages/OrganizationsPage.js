377c1259f0ba57e0df7271229c6a7021441cfc3f
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2022-12-01 14:15:39 +0100

    Redirect if user perms are not sufficient to show page (purely for useability)

--- src/pages/OrganizationsPage.js
+++ src/pages/OrganizationsPage.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 import OrganizationsTable from '../components/organizations/OrganizationsTable'
 export default function OrganizationsPage() {
+  const perms = useSelector((state) => state.user.loggedInUser.perms)
+  console.log(perms)
+  // Redirect if user accesses page directly via url and does not have permissions 
+  // (This is mainly for usability, authorization is of course handled by the api)
+  if (typeof perms !== 'undefined' ) {
+    if ( typeof perms.organizations === 'undefined' ) {
+      return <Navigate to="/" replace />
+    }
+  }
   return (
     <>
     <div className='table-container'>
